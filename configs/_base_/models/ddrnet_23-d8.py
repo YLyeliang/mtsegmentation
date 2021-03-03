e6e308fea6e894c755014a4d798b313d1492073a
YLyeliang <k87974@163.com>
2021-03-03 16:18:32 +0800

    update the ddrnet-23

---
+++ configs/_base_/models/ddrnet_23-d8.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*- 
+# @Time : 2021/3/3 3:52 下午 
+# @Author : yl
+
+norm_cfg = dict(type='BN', requires_grad=True)
+model = dict(
+    type='EncoderDecoder',
+    pretrained=None,
+    backbone=dict(
+        type='DualResNet',
+        depth=18,
+        augment=True,
+    ),
+    decode_head=dict(
+        type='FCNHead',
+        in_channels=256,
+        in_index=1,
+        channels=256,
+        num_convs=2,
+        concat_input=False,
+        dropout_ratio=0.1,
+        num_classes=2,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    auxiliary_head=dict(
+        type='FCNHead',
+        in_channels=128,
+        in_index=0,
+        channels=128,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=0.1,
+        num_classes=2,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)))
+# model training and testing settings
+train_cfg = dict()
+test_cfg = dict(mode='whole')
